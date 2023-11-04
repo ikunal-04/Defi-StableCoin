0f5574a946519b9eff908d366a0cd1391eae69e2
kunal <kunalgarg054@gmail.com>
2023-11-05 00:18:00 +0530

    Added functions to the DSCEngine contract

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.19;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+
 /**
  *@title DSCEngine
  *@author Kunal Garg
@@ -14,11 +16,80 @@ pragma solidity ^0.8.19;
  *
  * It is similar to DAI had no governance, no fees, and was only backed by WETH and WBTC.
  *
+ * Our DSC system should always be "overcollateralised". At no point, should the value of all collateral <= the $ backed value of all the DSC.
+ *
  * @notice This contract is the core of the DSC System. It handles all the logic for mining and remaining DSC, as well as depositing & withdrawing collateral.
  *@notice This contract is VERY lossely based on MakerDAO DSS (DAI) system.
  *
  */
 
 contract DSCEngine {
+    /** ERRORS */
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressAndPriceFeedAddressesMustBeSameLength();
+    error DSCEngine__NotAllowedToken();
 
+    /** STATE VARIABLES */
+    mapping(address token => address priceFeed) private s_priceFeeds; //tokentoPriceFeed
+
+    DecentralizedStableCoin private immutable i_dsc;
+
+    /** MODIFIERS */
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
+
+    /** FUNCTIONS */
+    constructor(
+        address[] memory tokenAddresses,
+        address[] memory priceFeedAddresses,
+        address dscAddress
+    ) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressAndPriceFeedAddressesMustBeSameLength();
+        }
+        // Here every collateral is backed to USD
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    function depositCollateralAndMintDSC() external {}
+
+    /**
+     * @param tokenCollateralAddress The address of the token to deposit as collateral.
+     * @param amountCollateral The amount of collateral to deposit.
+     */
+
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+    )
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+    {}
+
+    function redeemCollateralForDSC() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDSC() external {}
+
+    function burnDSC() external {}
+
+    function liquidate() external {}
+
+    function getHealthFactor() external {}
 }
